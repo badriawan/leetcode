a75f4c1a312b33b4cf454ab3ff93da6de18971c4
badriawan <117419515+badriawan@users.noreply.github.com>
2024-10-19 01:20:31 +0000

    new

---
+++ primenumber.cpp
@@ -0,0 +1,37 @@
+#include "primenumber.h"
+
+using namespace std;
+
+
+bool PrimeNumber::Process(int a){
+        
+        if(a<2){return false; }
+        if(a<=3){return true; }
+        if(a==5){return true; }
+
+
+        if(a%2==0 || a%3==0 || a%5==0 ){
+            return false;
+        }
+        else{
+
+            return true;
+        }
+
+
+    }
+
+int main(){
+
+    PrimeNumber p;
+    bool w = p.Process(5);
+    if(w==true){
+        cout<< "Prime Number"<<endl;
+    }
+    else{
+
+        cout<<"Not Prime Number"<<endl;
+
+    }
+
+}
